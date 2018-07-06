5294fd20891b7df2290428d73e03d2de364a867d
Debashish Nayak <debashishzero@gmail.com>
2018-07-06 12:58:35 +0530

    :cactus: added resume

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
-import { Grid, User, Mail, List } from 'react-feather';
+import { Grid, User, Mail, List, FileText } from 'react-feather';
 
 import Logo from '../../assets/images/my-name.png';
 
@@ -27,6 +27,9 @@ const Header = () => (
         <NavLink className="strike-through standout-text" activeClassName="active" to="/contact">
           contact
         </NavLink>
+        <a className="strike-through standout-text" target="_blank" rel="noopener noreferrer" href="https://drive.google.com/open?id=1FSRxWfKgX4ky7S3lR95XjHdvRWKgOdi_">
+          resume
+        </a>
       </div>
     </div>
     <div className="header__right hidden-above-mobile">
@@ -43,6 +46,9 @@ const Header = () => (
         <NavLink className="strike-through" activeClassName="active" to="/contact">
           <Mail className="float-right" size={20} />
         </NavLink>
+        <a className="strike-through" target="_blank" rel="noopener noreferrer" href="https://drive.google.com/open?id=1FSRxWfKgX4ky7S3lR95XjHdvRWKgOdi_">
+          <FileText className="float-right" size={20} />
+        </a>
       </div>
     </div>
   </nav>
